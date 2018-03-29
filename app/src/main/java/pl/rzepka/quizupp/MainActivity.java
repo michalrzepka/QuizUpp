4766e7acad382c4ae0e10cb8513a262b5513701f
Michal Rzepka <micrzepka@gmail.com>
2018-03-29 19:13:19 +0200

    Added final polish

--- app/src/main/java/pl/rzepka/quizupp/MainActivity.java
+++ app/src/main/java/pl/rzepka/quizupp/MainActivity.java
@@ -4,9 +4,12 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String name;
 
     public static final String EXTRA_NAME = "pl.rzepka.quizupp.NAME";
 
@@ -14,12 +17,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
     }
 
-    public void startQuiz(View view){
-        Intent intent = new Intent (this, Welcome.class);
+    public void startQuiz(View view) {
+        Intent intent = new Intent (this, QuizQuestions.class);
         EditText editText = (EditText) findViewById(R.id.name);
-        String name = editText.getText().toString().trim();
+        name = editText.getText().toString().trim();
         intent.putExtra(EXTRA_NAME, name);
         startActivity(intent);
     }
--- app/src/main/java/pl/rzepka/quizupp/QuizQuestions.java
+++ app/src/main/java/pl/rzepka/quizupp/QuizQuestions.java
@@ -1,28 +1,77 @@
 package pl.rzepka.quizupp;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioGroup;
 import android.widget.TextView;
-
-import org.w3c.dom.Text;
+import android.widget.Toast;
 
 public class QuizQuestions extends AppCompatActivity {
 
+    int score;
+    String name = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_welcome);
+        setContentView(R.layout.activity_questions);
 
         Intent intent = getIntent();
-        String name = intent.getStringExtra(MainActivity.EXTRA_NAME);
+        name = intent.getStringExtra(MainActivity.EXTRA_NAME);
 
         TextView textView = findViewById(R.id.welcome);
-        String welcomeMessage = String.format("Welcome, %s.");
+        String welcomeMessage = "Welcome, " + name + ".\nCheck your knowledge about Europe!";
         textView.setText(welcomeMessage);
-
     }
 
+    public int countPoints(){
 
+        score = 0;
+
+        CheckBox question_1_answer_A = (CheckBox) findViewById(R.id.question_1_answer_A);
+        CheckBox question_1_answer_B = (CheckBox) findViewById(R.id.question_1_answer_B);
+        CheckBox question_1_answer_C = (CheckBox) findViewById(R.id.question_1_answer_C);
+        CheckBox question_1_answer_D = (CheckBox) findViewById(R.id.question_1_answer_D);
+
+        boolean question_1_wrong_answers = question_1_answer_B.isChecked();
+
+        if (question_1_answer_A.isChecked() && !(question_1_wrong_answers)){ score+=1; } // adding 1 point per correct answer only if wrong answers are not checked
+        if (question_1_answer_C.isChecked() && !(question_1_wrong_answers)){ score+=1; }
+        if (question_1_answer_D.isChecked() && !(question_1_wrong_answers)){ score+=1; }
+
+        RadioGroup question_2 = (RadioGroup) findViewById(R.id.question_2_answers);
+        if (question_2.getCheckedRadioButtonId() == R.id.question_2_answer_C) { score+=3; }
+
+        CheckBox question_3_answer_A = (CheckBox) findViewById(R.id.question_3_answer_A);
+        CheckBox question_3_answer_B = (CheckBox) findViewById(R.id.question_3_answer_B);
+        CheckBox question_3_answer_C = (CheckBox) findViewById(R.id.question_3_answer_C);
+        CheckBox question_3_answer_D = (CheckBox) findViewById(R.id.question_3_answer_D);
+
+        boolean question_3_wrong_answers = question_3_answer_B.isChecked() || question_3_answer_D.isChecked();
+
+        if (question_3_answer_A.isChecked() && !(question_3_wrong_answers)){ score+=1; }
+        if (question_3_answer_C.isChecked() && !(question_3_wrong_answers)){ score+=1; }
+
+        RadioGroup question_4 = (RadioGroup) findViewById(R.id.question_4_answers);
+        if (question_4.getCheckedRadioButtonId() == R.id.question_4_answer_C) { score+=3; }
+
+        EditText question_5 = (EditText) findViewById(R.id.question_5_answer);
+        int question_5_answer = Integer.parseInt(question_5.getText().toString());
+        if (question_5_answer == 12) { score+=4; }
+
+        return score;
+    }
+
+    public void submitQuiz(View view) {
+        countPoints();
+        Context context = getApplicationContext();
+        String scoreMessage = "You scored " + score + "/15 points!";
+        Toast toast = Toast.makeText(context,scoreMessage,Toast.LENGTH_LONG);
+        toast.show();
+    }
 }
